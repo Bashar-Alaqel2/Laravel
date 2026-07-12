71c7983c74c30d765d14247d65aacc3f30126804
Bashar <basharmohammed946@gmail.com>
2026-07-13 02:41:04 +0300

    Fix playlist sync and suspended ads filtering

--- test_supa2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url2 = 'https://uyvykohckfygsbxbzrpp.supabase.co/storage/v1/object/public/ads/ads/Ru4HlqPYXMzSlQ04S0UPyzRmv88Klr5eUOpNIRuf.jpg';
-$ch2 = curl_init($url2);
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-$res2 = curl_exec($ch2);
-$code2 = curl_getinfo($ch2, CURLINFO_HTTP_CODE);
-echo "\nPublic REST Endpoint 2: Code $code2\n";
-echo "Body: " . substr($res2, 0, 50) . "...\n";
