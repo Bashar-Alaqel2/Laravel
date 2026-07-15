0a83cf2e4efecd448992ba0731d921a589e7c47a
Bashar <basharmohammed946@gmail.com>
2026-07-15 22:05:22 +0300

    إضافة مسار رئيسي لمنع نوم السيرفر

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
+Route::get('/', function() {
+    return response()->json(['status' => 'alive', 'message' => 'Render backend is awake!']);
+});
+
 Route::get('/clear-cache', function() {
     Artisan::call('optimize:clear');
     Artisan::call('migrate', ['--force' => true]);
